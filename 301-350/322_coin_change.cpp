82446ac3fb2f011b7c25edf31b7bd79d3eb31366
juneone <307980129@qq.com>
2021-01-26 10:18:01 +0800

    322_coin_change: basic dp

---
+++ 301-350/322_coin_change.cpp
@@ -0,0 +1,37 @@
+/*
+ * @see https://leetcode.com/problems/coin-change/
+ * 
+ * 换零钱: dp[i]表示换金额i的最少使用张数
+ * Input: coins = [1,2,5], amount = 11
+ * Output: 3
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+        	for (int j = 0; j < coins.size(); j++) {
+        		if (i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
+        			if (dp[i] == -1 || dp[i] > dp[i - coins[j]] + 1) {
+        				dp[i] = dp[i - coins[j]] + 1;
+        			}
+        		}
+        	}
+        }
+        return dp[amount];
+    }
+};
+
+int main()
+{
+    vector<int> coins = {1,2,5};
+    int amount = 11;
+    Solution s;
+    cout << s.coinChange(coins, amount) << endl;
+}
